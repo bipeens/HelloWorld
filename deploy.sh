677a1ce62b16529fd64d40cc808bf430ef272516
bipeens <bipeen.sawant@gslab.com>
2015-10-01 16:04:46 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e  
+sudo service start docker
 docker pull bipeen/container2 
+
 #docker stop docker_drone 
 #docker rm docker_drone  
 #docker run --name="docker_drone"  bipeen/docker_drone
